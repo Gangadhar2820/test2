25cd8a77c41724f287db5d323032e85882f142ca
Gangadhar <gangadhar2820@gmail.com>
2024-07-16 13:33:17 +0530

    testing

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import {BrowserRouter,Routes,Route}from "react-router-dom"
+import Home from './Home';
+import About from './About';
+
+function App() {
+  return <>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/about" element={<About/>}></Route>
+    <Route path="/home" element={<Home/>}></Route>
+  </Routes>
+  </BrowserRouter>
+  </>
+}
+
+export default App;
